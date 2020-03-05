3ce0f466ef2fbea1daee0c9c153ea5b8d285abae
Lars Grefer <lars.grefer@materna.de>
2020-03-05 20:49:50 +0100

    Initial Commit

---
+++ src/main/java/de/larsgrefer/tomcatexceptiondemo/TomcatExceptionDemoApplication.java
@@ -0,0 +1,13 @@
+package de.larsgrefer.tomcatexceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatExceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatExceptionDemoApplication.class, args);
+	}
+
+}
